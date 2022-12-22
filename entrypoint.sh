23710367c3a22177c4ad246c248d63bcfb9b0894
Steven Johns <10301400+duoi@users.noreply.github.com>
2022-12-22 23:03:47 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,16 @@ set -e
 
 install_zip_dependencies(){
 	echo "Installing and zipping dependencies..."
-	mkdir python
 	PIPENV_VENV_IN_PROJECT=1 pipenv install
-	pushd ./.venv/lib/python3.9/
-	zip -q -r ../../../dependencies.zip .
-	popd
+	mv .venv python
+	zip -q -r dependencies.zip ./python/lib/python3.9/site-packages
 }
 
 publish_dependencies_as_layer(){
 	echo "Publishing dependencies as a layer..."
 	local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
 	LAYER_VERSION=$(jq '.Version' <<< "$result")
-	rm -rf .venv
+	rm -rf python
 	rm dependencies.zip
 }
 
